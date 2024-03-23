efa561f251a57192105ceae80341087674d688f5
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-23 22:51:07 +0700

    sealed interface

---
+++ src/main/java/programmerzamannow/sealedclass/data/Circle.java
@@ -0,0 +1,9 @@
+package programmerzamannow.sealedclass.data;
+
+public non-sealed class Circle implements Shape{
+
+    @Override
+    public Long area(){
+        return null;
+    }
+}
---
+++ src/main/java/programmerzamannow/sealedclass/data/Shape.java
@@ -0,0 +1,5 @@
+package programmerzamannow.sealedclass.data;
+
+public sealed interface Shape permits Circle, Rectangle, Triangle{
+    Long area();
+}
